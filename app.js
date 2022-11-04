1866345452b1edb77ada35135174a51be93f81d9
Fakhirkhan098 <117271296+Fakhirkhan098@users.noreply.github.com>
2022-11-04 02:12:45 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,54 @@
+// var oops;
+// var com;
+// var ict;
+// var eng;
+// var maths;
+// var total;
+// var per;
+
+// oops = + prompt("enter your oops marks");
+// com = + prompt("enter your com marks");
+// ict = + prompt("enter your ict marks");
+// eng = + prompt("enter your eng marks");
+// maths = + prompt("enter your maths marks");
+
+// total = (oops + com + ict + eng + maths);
+// per = (total / 500) * 100;
+
+// console.log(oops);
+// console.log(com);
+// console.log(ict);
+// console.log(eng);
+// console.log(maths);
+// console.log(total);
+// console.log(per);
+
+
+// if (per >= 80) {
+//     console.log("Grade = A+");
+// }
+// else if (per < 80 && per >= 70) {
+//     console.log("Grade = A");
+// }
+// else if (per < 70 && per >= 60) {
+//     console.log("Grade = B");
+// }
+// else if (per < 60 && per >= 50) {
+//     console.log("Grade = C");
+// }
+// if (per < 50 && per >= 10) {
+//     console.log("Grade = F");
+// }
+
+// var age;
+// var studentcard;
+// age = prompt("Enter your age");
+// studentcard = prompt("studentcard");
+
+// if (age >= 18 && studentcard==true)
+// {
+//     console.log("Allow the Student");
+// }
+// else{
+// console.log("Not allow the student")
+// }
